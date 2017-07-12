d3a111820f3840bbe0c2f0e7c85ac7ab98313b3f
Luis Lebolo <lebolo@users.noreply.github.com>
2017-07-12 12:41:00 -0400

    Update run_twistd.sh
    _twistd_ changed the way `--port` works in versions > 16.6.0
    
    see https://github.com/robgjansen/onionperf/issues/26#issuecomment-306858519 for details

--- run_twistd.sh
+++ run_twistd.sh
@@ -2,4 +2,4 @@ export AWS_PROFILE=h3pipelines
 export AWS_DEFAULT_PROFILE=h3pipelines
 unset AWS_ACCESS_KEY_ID
 unset AWS_SECRET_ACCESS_KEY
-twistd web --port 8080 --wsgi s3proxy.app.app
+twistd -n web --port tcp:port=8080 --wsgi s3proxy.app.app
