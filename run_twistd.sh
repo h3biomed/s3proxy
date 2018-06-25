87ee64942606eb548f6e659a0f6fe1e6b9081253
Luis Lebolo <lebolo@users.noreply.github.com>
2018-06-25 15:04:52 -0400

    Update run_twistd.sh
    AWS keys are taken from your env, not from any file, so you'll have to use `set` before running this

--- run_twistd.sh
+++ run_twistd.sh
@@ -1,5 +1,1 @@
-export AWS_PROFILE=h3pipelines
-export AWS_DEFAULT_PROFILE=h3pipelines
-unset AWS_ACCESS_KEY_ID
-unset AWS_SECRET_ACCESS_KEY
 twistd -n web --port tcp:port=8080 --wsgi s3proxy.app.app
